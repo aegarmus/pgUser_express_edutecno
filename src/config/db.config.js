406b15a2609f7ad6511989d8319731f4fee70451
aegarmus <aegarmus@gmail.com>
2024-12-02 21:54:33 -0300

    feat: create user system integrated

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,4 +1,9 @@
-import { Pool } from 'pg';
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pkg
 
 const pool = new Pool({
     host: process.env.DB_HOST,
@@ -8,5 +13,10 @@ const pool = new Pool({
     database: process.env.DB_NAME
 })
 
-
+/**
+ * Esta función ejecuta la query entregada en su parametro text y ejecuta esta petición en la DB en base a los datos entregados en su params
+ * @param {string} text - Estructura en formato string de la query que se quiere mandar a la DB 
+ * @param {Array<any>} params - Lista de datos que se van a implementar en la query a la DB
+ * @returns 
+ */
 export const query = (text, params) => pool.query(text, params) 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import express, { urlencoded } from 'express';
-import dotenv from 'dotenv';
 
-dotenv.config();
+import UserRouter from './routes/usuario.route.js'
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
+
+
+app.use('/api/v1', UserRouter);
 
 
 app.listen(PORT, () => {
